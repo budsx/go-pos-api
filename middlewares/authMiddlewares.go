a4056b675778425cb2bf42b5d1c9194cfc88b007
Etvendy Afrilio <etvendy@gmail.com>
2022-09-19 15:59:04 +0700

    Feat(users) : add middleware

---
+++ middlewares/authMiddlewares.go
@@ -0,0 +1,41 @@
+package middlewares
+
+import (
+	"go-pos-api/helpers"
+	"go-pos-api/services"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(userServices services.UserServices, authService services.AuthService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helpers.APIResponse(http.StatusUnauthorized, "Error", "Unauthorized", "Dont Have Authorization")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		// Bearer token
+		tokenString := ""
+		arrayToken := strings.Split(authHeader, " ")
+		if len(arrayToken) == 2 {
+			tokenString = arrayToken[1]
+		}
+		result, userId, err := authService.ValidateToken(tokenString)
+		if err != nil && !result && userId == 0 {
+			response := helpers.APIResponse(http.StatusUnauthorized, "Error", "Unauthorized", "Dont Have Authorization")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		} else {
+			user, err := userServices.GetUsersByID(userId)
+			if err != nil {
+				response := helpers.APIResponse(http.StatusUnauthorized, "Error", "Unauthorized", "Dont Have Authorization")
+				c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+				return
+			}
+			c.Set("currentUser", user)
+		}
+	}
+}
