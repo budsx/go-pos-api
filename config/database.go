39d9c369a876a5bff88d6af93d5e18809dabbe62
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-09-22 11:38:57 +0700

    hotfix db connection

--- config/database.go
+++ config/database.go
@@ -4,21 +4,20 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewConnection() *gorm.DB {
-	godotenv.Load()
+	// godotenv.Load()
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbAddress := os.Getenv("DB_ADDRESS")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dbURL := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", dbUser, dbPassword, dbAddress, dbPort, dbName)
+	dbURL := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=require", dbUser, dbPassword, dbAddress, dbPort, dbName)
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
