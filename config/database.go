d749ff8b9cf9fb5fb21518a6842c0498efd50ae3
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-09-22 11:06:19 +0700

    remove unused file and define route

--- config/database.go
+++ config/database.go
@@ -1,13 +1,26 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewConnection() *gorm.DB {
-	db, err := gorm.Open(postgres.Open("postgres://postgres:postgres@localhost:5432/go-pos-api?sslmode=disable"), &gorm.Config{})
+	godotenv.Load()
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbAddress := os.Getenv("DB_ADDRESS")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dbURL := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", dbUser, dbPassword, dbAddress, dbPort, dbName)
+
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
